02efaaaddc8fe965113e89c3180ea879846ab3c9
ali-asghar-sh <ali.asghar.sh@live.com>
2025-07-26 13:00:43 +0800

    these versions work

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,8 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
-
-  name = "ali-vpc-ecr-ecs"
-  cidr = "10.0.0.0/16"
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "6.0.1"
+  name    = "ali-vpc-ecr-ecs"
+  cidr    = "10.0.0.0/16"
 
   azs             = ["ap-southeast-1", "ap-southeast-2", "ap-southeast-3"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
